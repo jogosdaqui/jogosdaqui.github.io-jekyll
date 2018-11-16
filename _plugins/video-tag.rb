265880bcab4b7ffcfa8bfff2d6221612f7156751
giacomelli <giacomelli@gmail.comi>
2018-11-16 12:17:08 -0200

    Organizados os posts em pastas por ano e movidas as imagens de images/galleries para assets

--- _plugins/video-tag.rb
+++ _plugins/video-tag.rb
@@ -10,7 +10,7 @@ module Jekyll
       site = context.registers[:site]
       
       "<video width='640' height='360' autoplay muted loop>
-          <source src='#{site.baseurl}/images/galleries/#{galleryDir}/#{@text}' type='video/mp4'>
+          <source src='#{site.baseurl}/assets/#{galleryDir}/#{@text}' type='video/mp4'>
           Your browser does not support the video tag.
        </video>"
     end
