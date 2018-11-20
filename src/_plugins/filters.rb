412df4b513ad8076b35dcc7406b4f3b3e7ac973c
giacomelli <giacomelli@gmail.comi>
2018-11-20 20:33:18 -0200

    Issue #10: Dividir o resultado da página de tags por tipo de categoria dos posts
    Closes #10

--- src/_plugins/filters.rb
+++ src/_plugins/filters.rb
@@ -3,6 +3,29 @@ module Jekyll
     def galleryname(input)
       GalleryGenerator::getGalleryName(input)
     end
+
+    def categorytitle(input)
+      case input[0]
+      when 'News'
+        'Notícias'
+      when 'Preview'
+        'Prévias'
+      when 'News'
+        'Notícias'
+      when 'Game'
+        'Jogos'
+      when 'Interview'
+        'Entrevistas'
+      when 'Events'
+        'Eventos'
+      when 'Column'
+        'Colunas'
+      when 'Promotion'
+        'Promoções'
+      else
+        input
+      end
+    end
   end
 end
   
