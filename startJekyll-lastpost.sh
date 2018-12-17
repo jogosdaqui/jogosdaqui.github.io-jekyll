3893ed7c39e5611d26b5bf753af7783e42846be7
giacomelli <giacomelli@gmail.com>
2018-12-17 18:32:00 -0200

    Alterados os shells para considerar o arquivo style.css que só é gerado ao final
    Ainda preciso descbrir uma forma de configurar o Jekyll para que isso não seja necessário

--- startJekyll-lastpost.sh
+++ startJekyll-lastpost.sh
@@ -1,3 +1,8 @@
+echo Starting...
 cd src
-bundle exec jekyll serve --future --limit_posts 1
+mkdir _site &>/dev/null
+mkdir _site/libs &>/dev/null
+echo "" > _site/libs/style.css
+bundle exec jekyll build --future --limit_posts 1 >/dev/null
+bundle exec jekyll serve --incremental --future --limit_posts 1
 cd ..
--- startJekyll.sh
+++ startJekyll.sh
@@ -1,3 +1,8 @@
+echo Starting...
 cd src
-bundle exec jekyll serve --future
+mkdir _site &>/dev/null
+mkdir _site/libs &>/dev/null
+echo "" > _site/libs/style.css
+bundle exec jekyll build --incremental
+bundle exec jekyll serve --incremental
 cd ..
